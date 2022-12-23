0ed9bd109990d1e38c75af12f29234fc9d0041c1
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2022-12-23 17:25:56 +0300

    started.

---
+++ 01-hello-world.js
@@ -0,0 +1,10 @@
+// *********** merhaba dünya örnekleri *************
+
+// Dokumana(document) yazdırma işlemi ile merhaba dünya:
+//document.write("Merhaba Dunya");
+
+// Uyarı mesaji (Alert) ile merhaba dunya:
+//alert("merhaba Dunya")
+
+//console.log ile merhaba dunya:
+//console.log("Merhaba Dunya")
