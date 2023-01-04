81248ec9fbb480b256104d3b7255d9a2259a5bc8
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-01-04 23:07:09 +0300

    fixed.

---
+++ 12-liste-icindeki-ogeye-ulaşmak-veya-yeni-oge-eklemek.js
@@ -0,0 +1,13 @@
+let lastChild = document.querySelector("ul#list>li:last:child")
+lastChild.innerHTML = "son oge degisti..."
+
+let firstChild = document.querySelector("ul#list>li:first:child")
+firstChild.innerHTML = "ilk oge degisti..."
+
+let ulDOM = document.querySelector("ul#list")
+let liDOM = document.createElement('li')
+
+li.liDOM.innerHTML = "Kendi Olusturdugumuz Oge"
+
+// ul.DOM.append(liDOM) // en sona ekler
+ulDOM.prepend(liDOM)
