3fadf59f719b47f1d29ddb65c3e1ca7d238369ed
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2023-01-09 03:27:06 +0300

    added.

---
+++ 35-while-dongusu-kullanimi.js
@@ -0,0 +1,14 @@
+
+//let counter = 0;
+
+//while ( counter < 10 ) {
+//    console.log(counter)
+//    counter ++
+//}
+
+let userName = ""
+
+while(!userName){
+    userName = prompt("Kullanici Adini Giriniz: ")
+    console.log(userName)
+}
